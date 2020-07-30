d070c0e2293c6d4db7242b67287c4fb4160e1307
Richard Pacheco <richardsp.dev@gmail.com>
2020-07-30 01:23:44 -0300

    feat: add 404 page template (wip)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import Home from './pages/home';
 import CadastroVideo from './pages/cadastro/video'
 import CadastroCategoria from './pages/cadastro/categoria'
+import NotFound from './pages/not-found'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,7 +14,7 @@ ReactDOM.render(
       <Route path="/cadastro/categoria" exact component={CadastroCategoria} />
       <Route path="/cadastro/video" exact component={CadastroVideo} />
       <Route path="/" exact component={Home} />
-      <Route component={() => (<h1>404</h1>)} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Layout from '../../components/Layout'
+
+function NotFound() {
+  return (
+    <Layout>
+      <FlexWrapper>
+        <LargeFriendlyLetters>
+          404
+        </LargeFriendlyLetters>
+        <h2>Come back later, maybe?</h2>
+      </FlexWrapper>
+    </Layout>
+  )
+}
+
+const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const LargeFriendlyLetters = styled.h1`
+  margin: 2rem 0;
+  font-size: 5rem;
+`
+
+export default NotFound
