a717bfa28b7eef39552d9c0ac3eccabec0b9d1e8
Richard Pacheco <richardsp.dev@gmail.com>
2020-08-03 16:50:31 -0300

    refactor: update api url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL = window.location.hostname.includes('localhost')
   ? 'http://localhost:3003'
-  : 'https://gastroflix-api.herokuapp.com';
+  : 'https://gastroflix-legacy.herokuapp.com';
 
 export default {
   URL,
